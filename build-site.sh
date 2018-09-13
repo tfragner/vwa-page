2b47ef0e2cee3b21c0a0f20817a868f59de54177
Thomas Fragner <fragner@bitter.at>
2018-09-13 20:47:59 +0200

    modifications to scripts

--- build-site.sh
+++ build-site.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
 cp -purv .pandoc ~/
 cd content/doc
+python3.6 -m venv venv
 source venv/bin/activate
+pip install -r .pandoc/requirements.txt
 bin/compile-all.sh
 cd ../../
 hugo
+rm -rf content/doc/venv
--- run-server.sh
+++ run-server.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 cp -purv .pandoc ~/
 cd content/doc
+python3.6 -m venv venv
 source venv/bin/activate
+pip install -r .pandoc/requirements.txt
 cd ../../
 hugo server --disableFastRender --bind=0.0.0.0
+rm -rf content/doc/venv
